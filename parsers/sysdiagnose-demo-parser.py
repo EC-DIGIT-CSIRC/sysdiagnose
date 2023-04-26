237f5b4993078c3a1bfd6b7ff927ccd39951d648
David Durvaux <david@autopsit.org>
2023-04-26 22:52:16 +0200

    Add a demo parser

---
+++ parsers/sysdiagnose-demo-parser.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python3
+
+# For Python3
+# Demo blank parsers
+# Author: david@autopsit.org
+
+import os
+import sys
+import json
+from optparse import OptionParser
+import time
+import struct
+import datetime
+
+version_string = "sysdiagnose-demo-parser.py v2023-04-26 Version 1.0"
+
+# ----- definition for parsing.py script -----#
+
+parser_description = "Demo parsers"
+parser_input = "demo_input_file"
+parser_call = "demo_function"
+
+# --------------------------------------------#
+
+# --------------------------------------------------------------------------- #
+
+
+def demo_function(filepath, ios_version=16):
+    """
+        This is the function that will be called
+    """
+    json_object = {}
+    return json_object
+
+
+# --------------------------------------------------------------------------- #
+
+def main():
+    """
+        Main function, to be called when used as CLI tool
+    """
+    if sys.version_info[0] < 3:
+        print("Must be using Python 3! Exiting ...")
+        exit(-1)
+
+    print("Running " + version_string + "\n")
+
+    usage = "\n%prog -i inputfile\n"
+
+    parser = OptionParser(usage=usage)
+    parser.add_option("-i", dest="inputfile",
+                      action="store", type="string",
+                      help="Demo file to use")
+    (options, args) = parser.parse_args()
+
+    # no arguments given by user, print help and exit
+    if len(sys.argv) == 1:
+        parser.print_help()
+        exit(-1)
+
+    # Call the demo function when called directly from CLI
+    print(demo_function(options.inputfile))
+
+# --------------------------------------------------------------------------- #
+
+
+"""
+   Call main function
+"""
+if __name__ == "__main__":
+
+    # Create an instance of the Analysis class (called "base") and run main
+    main()
+
+# That's all folk ;)
