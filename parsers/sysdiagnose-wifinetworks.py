e0ce39618d553d40394d838a739f2b5e80687269
Emilien <emilien@cert.europa.eu>
2023-05-22 11:29:09 +0200

    moving plist functions to misc

--- parsers/sysdiagnose-wifinetworks.py
+++ parsers/sysdiagnose-wifinetworks.py
@@ -25,6 +25,8 @@ import glob
 import re
 import datetime
 import binascii
+sys.path.append('..')
+from sysdiagnose import misc
 
 # ----- definition for parsing.py script -----#
 
@@ -42,39 +44,8 @@ def parsewifinetwork(wifi_data):
             with open(data, 'r') as f:
                 output['recent_networks'] = json.load(f)
         if data.endswith('.plist'):
-            output[data] = load_plist_and_fix(data)
+            output[data] = misc.load_plist_and_fix(data)
     return output
-
-def load_plist_and_fix(plist):
-    with open(plist, 'rb') as f:
-        plist = plistlib.load(f)
-        plist = find_datetime(plist)
-        plist = find_bytes(plist)
-    return plist
-
-def find_datetime(d):
-    for k, v in d.items():
-        if isinstance(v, dict):
-            find_datetime(v)
-        elif isinstance(v, list):
-            for item in v:
-                if isinstance(item, dict):
-                    find_datetime(item)
-        elif isinstance(v, datetime.datetime):
-            d[k]=v.isoformat()
-    return d
-
-def find_bytes(d):
-    for k, v in d.items():
-        if isinstance(v, dict):
-            find_bytes(v)
-        elif isinstance(v, list):
-            for item in v:
-                if isinstance(item, dict):
-                    find_bytes(item)
-        elif isinstance(v, bytes):
-            d[k]=binascii.hexlify(v).decode('utf-8')
-    return d
 
 # --------------------------------------------------------------------------- #
 
