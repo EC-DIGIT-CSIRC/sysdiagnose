41cbe82daac9f8644016aacb7e9d95e8a7b1e940
Christophe Vandeplas <christophe@vandeplas.com>
2024-04-30 11:45:46 +0200

    new: [test] parser wifinetworks

---
+++ tests/test_misc.py
@@ -0,0 +1,19 @@
+import unittest
+import misc
+
+
+class TestMisc(unittest.TestCase):
+
+    def test_load_plist_with_binary_file(self):
+        result = misc.load_plist_as_json("tests/testdata/iOS15/sysdiagnose_2023.05.24_13-29-15-0700_iPhone-OS_iPhone_19H349/WiFi/com.apple.wifi.known-networks.plist")
+        self.assertGreater(len(result), 0)
+        self.assertTrue(result['wifi.network.ssid.FIRSTCONWIFI'])
+
+    def test_load_plist_with_json_file(self):
+        result = misc.load_plist_as_json("tests/testdata/iOS15/sysdiagnose_2023.05.24_13-29-15-0700_iPhone-OS_iPhone_19H349/logs/SystemVersion/SystemVersion.plist")
+        self.assertGreater(len(result), 0)
+        self.assertTrue(result['ProductName'])
+        self.assertTrue(result['ProductBuildVersion'])
+        self.assertTrue(result['ProductVersion'])
+        self.assertTrue(result['BuildID'])
+        self.assertTrue(result['SystemImageID'])
---
+++ tests/test_parsers_wifinetworks.py
@@ -0,0 +1,31 @@
+import os
+import unittest
+
+from parsers.wifinetworks import parsewifinetwork
+
+
+class TestParsersWifiNetworks(unittest.TestCase):
+
+    log_root_path = 'tests/testdata/iOS15/sysdiagnose_2023.05.24_13-29-15-0700_iPhone-OS_iPhone_19H349/'
+    log_files = [
+        "WiFi/com.apple.wifi.known-networks.plist",
+        "WiFi/com.apple.wifi.plist",
+        "WiFi/LEGACY_com.apple.wifi-networks.plist",
+        "WiFi/com.apple.wifi-private-mac-networks.plist",
+        "WiFi/com.apple.wifi.recent-networks.json"
+    ]
+
+    def test_parsewifinetwork(self):
+        files = [os.path.join(self.log_root_path, log_file) for log_file in self.log_files]
+        result = parsewifinetwork(files)
+        self.assertGreater(len(result), 0)
+        for log_file in self.log_files:
+            end_of_filename = log_file.split('/')[-1]
+            self.assertTrue(end_of_filename in result)
+            if 'LEGACY' in end_of_filename:  # skip empty file
+                continue
+            self.assertGreater(len(result[end_of_filename]), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
