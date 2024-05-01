3e1b674360871b09e7d6c4c4ec1e9eee80cec685
Christophe Vandeplas <christophe@vandeplas.com>
2024-05-01 09:00:36 +0200

    new: [test] olddsc test with rewrite

---
+++ tests/test_parsers_olddsc.py
@@ -0,0 +1,21 @@
+from parsers.olddsc import parse_olddsc_file, get_log_files
+from tests import SysdiagnoseTestCase
+import unittest
+
+
+class TestParsersOlddsc(SysdiagnoseTestCase):
+
+    def test_parse_olddsc_file(self):
+        for log_root_path in self.log_root_paths:
+            files = [log_file for log_file in get_log_files(log_root_path)]
+            for file in files:
+                print(f'Parsing {file}')
+                result = parse_olddsc_file(file)
+                self.assertTrue('Unslid_Base_Address' in result)
+                self.assertTrue('Cache_UUID_String' in result)
+                self.assertTrue('Binaries' in result)
+                self.assertTrue(len(result['Binaries']) > 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
