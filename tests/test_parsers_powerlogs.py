82b94ef0e4e166f05c90ef018b5632255f512bf3
Christophe Vandeplas <christophe@vandeplas.com>
2024-05-01 08:39:33 +0200

    new: [test] powerlogs

---
+++ tests/test_parsers_powerlogs.py
@@ -0,0 +1,18 @@
+from parsers.powerlogs import get_powerlogs, get_log_files
+from tests import SysdiagnoseTestCase
+import unittest
+
+
+class TestParsersPowerlogs(SysdiagnoseTestCase):
+
+    def test_get_powerlogs(self):
+        for log_root_path in self.log_root_paths:
+            files = [log_file for log_file in get_log_files(log_root_path)]
+            for file in files:
+                print(f'Parsing {file}')
+                result = get_powerlogs(file)
+                self.assertTrue('sqlite_sequence' in result)
+
+
+if __name__ == '__main__':
+    unittest.main()
