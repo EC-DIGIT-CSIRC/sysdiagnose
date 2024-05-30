55e6319386e8e47bca50390f8ce53c1d4cbe51c8
Christophe Vandeplas <christophe@vandeplas.com>
2024-05-30 12:04:23 +0200

    new: [parsers] plist and remotectl_dumpstate parser

---
+++ parsers/plists.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import glob
+import misc
+import os
+
+
+def get_log_files(log_root_path: str) -> list:
+    log_files_globs = [
+        '**/*.plist'
+    ]
+    log_files = []
+    for log_files_glob in log_files_globs:
+        log_files.extend(glob.glob(os.path.join(log_root_path, log_files_glob)))
+
+    return log_files
+
+
+def parse_path(path: str) -> list | dict:
+    return misc.load_plist_file_as_json(path)
+
