8b4325229a3d2ab4305e55cc1e733e86bc9283ff
Christophe Vandeplas <christophe@vandeplas.com>
2024-05-31 11:44:21 +0200

    new: [parsers] stub code for crashlogs parser #70

---
+++ parsers/crashlogs.py
@@ -0,0 +1,34 @@
+import glob
+import os
+'''
+
+# FIXME Have a look at the interesting evidence first, see which files are there that are not on other devices
+- crashes_and_spins folder
+  - ExcUserFault file
+- crashes_and_spins/Panics subfolder
+- summaries/crashes_and_spins.log
+
+Though one as there is not necessary a fixed structure
+- first line is json
+- rest depends ...
+
+Or perhaps include that in a normal log-parser.
+And do the secret magic in the hunting rule
+'''
+
+parser_description = "Parsing crashes folder"
+
+
+def get_log_files(log_root_path: str) -> list:
+    log_files_globs = [
+        'crashes_and_spins/*.ips'
+    ]
+    log_files = []
+    for log_files_glob in log_files_globs:
+        log_files.extend(glob.glob(os.path.join(log_root_path, log_files_glob)))
+
+    return log_files
+
+
+def parse_path(path: str) -> list | dict:
+    raise NotImplementedError("not implemented yet")
