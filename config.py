070536991c13d2eceb45a89660dc7d8bd5bc5e44
Christophe Vandeplas <christophe@vandeplas.com>
2024-06-05 11:40:33 +0200

    fix: fixes parts of relative paths issue #49

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
-
+import os
 
 """
 This file contains all of the configuration values for the project.
 """
 
 
-cases_file = "cases.json"
-data_folder = "./data/"
-parsed_data_folder = "./parsed_data/"
-parsers_folder = "./parsers/"
-analysers_folder = "./analysers"
+config_folder = os.path.dirname(os.path.abspath(__file__))
+
+cases_file = os.path.join(os.curdir, "cases.json")
+data_folder = os.path.join(config_folder, "data")
+parsed_data_folder = os.path.join(os.curdir, "parsed_data")  # stay in current folder
+parsers_folder = os.path.join(config_folder, "parsers")
+analysers_folder = os.path.join(config_folder, "analysers")
 debug = True
