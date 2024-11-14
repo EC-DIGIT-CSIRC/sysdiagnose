023e0cba5d8c0a8f462c82b66eb7ef8a3c8fde73
Christophe Vandeplas <christophe@vandeplas.com>
2024-11-14 13:36:51 +0100

    new: [mcstate] new mcstate shared profile parser

---
+++ src/sysdiagnose/parsers/mcstate_shared_profile.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python3
+
+import glob
+import os
+from sysdiagnose.utils.base import BaseParserInterface
+import sysdiagnose.utils.misc as misc
+from datetime import datetime, timezone
+
+
+class McStateSharedProfileParser(BaseParserInterface):
+    description = "Parsing MCState Shared Profile stub files"
+    format = "jsonl"  # by default json
+
+    def __init__(self, config: dict, case_id: str):
+        super().__init__(__file__, config, case_id)
+
+    def get_log_files(self) -> list:
+        log_files_globs = [
+            "logs/MCState/Shared/profile-*.stub"
+        ]
+        log_files = []
+        for log_files_glob in log_files_globs:
+            for item in glob.glob(os.path.join(self.case_data_subfolder, log_files_glob)):
+                if os.path.getsize(item) > 0:
+                    log_files.append(item)
+        return log_files
+
+    def execute(self) -> list | dict:
+        '''
+        this is the function that will be called
+        '''
+        result = []
+        log_files = self.get_log_files()
+        for log_file in log_files:
+            entry = misc.load_plist_file_as_json(log_file)
+            timestamp = datetime.strptime(entry['InstallDate'], '%Y-%m-%dT%H:%M:%S.%f')
+            timestamp = timestamp.replace(tzinfo=timezone.utc)  # ensure timezone is UTC
+            entry['datetime'] = timestamp.isoformat(timespec='microseconds')
+            entry['timestamp'] = timestamp.timestamp()
+            result.append(entry)
+
+        return result
---
+++ tests/test_parsers_mcstatesharedprofile.py
@@ -0,0 +1,23 @@
+from sysdiagnose.parsers.mcstate_shared_profile import McStateSharedProfileParser
+from tests import SysdiagnoseTestCase
+import unittest
+import os
+
+
+class TestParsersMcSettingsEvents(SysdiagnoseTestCase):
+
+    def test_parse_mcsettingsevents(self):
+        for case_id, case in self.sd.cases().items():
+            p = McStateSharedProfileParser(self.sd.config, case_id=case_id)
+            files = p.get_log_files()  # noqa F841
+
+            p.save_result(force=True)
+            self.assertTrue(os.path.isfile(p.output_file))
+
+            result = p.get_result()
+            for item in result:
+                self.assertTrue('timestamp' in item)
+
+
+if __name__ == '__main__':
+    unittest.main()
