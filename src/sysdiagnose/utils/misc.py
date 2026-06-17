82a79d976fa85cafb71c6a272ba7db4d075d9d8d
Christophe Vandeplas <christophe@vandeplas.com>
2026-06-17 15:06:24 +0200

    fix: [parser] plist exception propagates and is caught higher up

--- src/sysdiagnose/utils/misc.py
+++ src/sysdiagnose/utils/misc.py
@@ -60,13 +60,10 @@ def get_version(filename="VERSION.txt"):
 
 def load_plist_file_as_json(fname: str) -> dict:
     if os.path.getsize(fname) == 0:
-        return {"error": ["Empty file"]}
-    try:
-        with open(fname, "rb") as f:
-            plist = nska_deserialize.deserialize_plist(f, full_recurse_convert_nska=True, format=dict)
-            return json_serializable(plist)
-    except Exception:
-        return {"error": ["Invalid plist file"]}
+        return {}
+    with open(fname, "rb") as f:
+        plist = nska_deserialize.deserialize_plist(f, full_recurse_convert_nska=True, format=dict)
+        return json_serializable(plist)
 
 
 def load_plist_string_as_json(plist_string: str) -> dict:
